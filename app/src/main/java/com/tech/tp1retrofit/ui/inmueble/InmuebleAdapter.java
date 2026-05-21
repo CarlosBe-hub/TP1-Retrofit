1818da6390d145c740813695f70a5b6510ba9e7f
Agustin-Benenatti <agus.benenatti09@gmail.com>
2026-05-21 17:38:54 -0300

    Correccion del adapterInmueble
    Se termino de hacer el adaptar, ya que faltaba el model de inmuebles y se habia dejado incompleto..

--- app/src/main/java/com/tech/tp1retrofit/ui/inmueble/InmuebleAdapter.java
+++ app/src/main/java/com/tech/tp1retrofit/ui/inmueble/InmuebleAdapter.java
@@ -1,24 +1,59 @@
 package com.tech.tp1retrofit.ui.inmueble;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.tech.tp1retrofit.R;
+import com.tech.tp1retrofit.data.model.Inmueble;
+import com.tech.tp1retrofit.databinding.ItemInmuebleBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class InmuebleAdapter extends RecyclerView.Adapter<InmuebleAdapter.ViewHolder>{
+
+    private List<Inmueble> inmuebles = new ArrayList<>();
+
     @NonNull
     @Override
     public InmuebleAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        ItemInmuebleBinding binding = ItemInmuebleBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+        );
+        return new ViewHolder(binding);
+    }
+    public void setInmuebles(List<Inmueble> inmuebles) {
+        this.inmuebles = inmuebles;
+        notifyDataSetChanged();
     }
 
     @Override
     public void onBindViewHolder(@NonNull InmuebleAdapter.ViewHolder holder, int position) {
-
+        Inmueble inmueble = inmuebles.get(position);
+        holder.binding.tvDireccion.setText(inmueble.getDireccion());
+        holder.binding.tvPrecio.setText("$" + inmueble.getPrecio());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return inmuebles.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ItemInmuebleBinding binding;
+
+        public ViewHolder(@NonNull ItemInmuebleBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
     }
 }
--- app/src/main/java/com/tech/tp1retrofit/ui/inmueble/InmuebleFragment.java
+++ app/src/main/java/com/tech/tp1retrofit/ui/inmueble/InmuebleFragment.java
@@ -7,11 +7,40 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import com.tech.tp1retrofit.R;
+import com.tech.tp1retrofit.data.model.Inmueble;
+import com.tech.tp1retrofit.databinding.FragmentInmueblesBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InmuebleFragment extends Fragment {
-    public View onCreate(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedIntanceState ){
-        return inflater.inflate(R.layout.fragment_inmuebles, container, false);
+    private FragmentInmueblesBinding binding;
+    private InmuebleAdapter inmuebleAdapter;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedIntanceState ){
+        binding = FragmentInmueblesBinding.inflate(inflater,container,false);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        binding.rvInmuebles.setLayoutManager(gridLayoutManager);
+
+        inmuebleAdapter = new InmuebleAdapter();
+        binding.rvInmuebles.setAdapter(inmuebleAdapter);
+
+        //Hay que borrar la harcodeada..
+        List<Inmueble> listaDePrueba = new ArrayList<>();
+
+        listaDePrueba.add(new Inmueble("Salta 325", 3, true, 1, "", 17000.0, null, 1, 1200, "Casa", "Residencial"));
+        listaDePrueba.add(new Inmueble("Lavalle 450", 2, true, 2, "", 25000.0, null, 1, 800, "Departamento", "Residencial"));
+        listaDePrueba.add(new Inmueble("Belgrano 218", 5, true, 3, "", 90000.0, null, 2, 3500, "Casa", "Residencial"));
+        listaDePrueba.add(new Inmueble("San Martín 102", 1, true, 4, "", 35000.0, null, 2, 40, "Local", "Comercial"));
+
+        inmuebleAdapter.setInmuebles(listaDePrueba);
+
+        binding.fabAgregarInmueble.setOnClickListener(v ->{
+
+        });
+        return binding.getRoot();
     }
 }
